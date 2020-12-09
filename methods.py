d3c4570b9c43c2f8dff67759f2eee6d2907956af
Josh <joshuaarkirk@hotmail.co.uk>
2020-12-09 22:11:24 +0000

    added the french flag

--- methods.py
+++ methods.py
@@ -1,11 +1,10 @@
 from rpi_ws281x import *
 
-def tricolour(strip, num_leds: int):
+def tricolour(strip):
     """Wipe color across display a pixel at a time."""
-    each_part = num_leds // 3
+    each_part = strip.numPixels() // 3
     print(each_part)
     for led in range(each_part):
-        print(led)
         strip.setPixelColorRGB(led, 255, 0, 0)
         strip.setPixelColorRGB(led+each_part, 255, 255, 255)
         strip.setPixelColorRGB(led+(each_part*2), 0, 0, 255)
