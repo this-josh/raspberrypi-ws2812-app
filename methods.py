6d42e88fd11c0d8cf7503250aa97ec4aa0b0aca4
Josh <joshuaarkirk@hotmail.co.uk>
2020-12-09 17:53:27 +0000

    Need to show colour

--- methods.py
+++ methods.py
@@ -9,4 +9,4 @@ def tricolour(strip, num_leds: int):
         strip.setPixelColorRGB(led, 255, 0, 0)
         strip.setPixelColorRGB(led+each_part, 255, 255, 255)
         strip.setPixelColorRGB(led+(each_part*2), 0, 0, 255)
-
+    strip.show()
