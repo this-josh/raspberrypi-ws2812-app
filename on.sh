d721a38345d33e241d6e6b33b6b6c59fca4aa9fe
Josh <joshuaarkirk@hotmail.co.uk>
2020-12-12 18:26:57 +0000

    trying to sort out service

--- on.sh
+++ on.sh
@@ -1,5 +1,17 @@
-cd /src/git/raspberrypi
+# cd /src/git/raspberrypi
 echo activating env
-source lights_app/bin/activate
+# source lights_app/bin/activate
 echo starting server
-sudo lights_app/bin/python3 build_app.py
+# sudo lights_app/bin/python3 build_app.py
+
+# sudo /home/src/git/raspberrypi/lights_app/bin/python3 build_app.py/
+
+
+# cd /lib/systemd/system/
+
+# source /src/git/raspberrypi/on.sh
+# sudo systemctl stop lights.service
+# sudo systemctl daemon-reload
+# sudo systemctl enable lights.service
+# sudo systemctl start lights.service
+# systemctl status lights.service
