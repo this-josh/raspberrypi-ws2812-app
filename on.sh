518bac08a08b384273f4a11bc0a4e5e09da7f33e
Josh <joshuaarkirk@hotmail.co.uk>
2020-12-12 18:37:25 +0000

    remove debugging code

--- on.sh
+++ on.sh
@@ -3,18 +3,3 @@ echo activating env
 source lights_app/bin/activate
 echo starting server
 sudo lights_app/bin/python3 build_app.py
-
-# sudo /home/src/git/raspberrypi/lights_app/bin/python3 build_app.py/
-
-
-# cd /lib/systemd/system/
-
-# source /src/git/raspberrypi/on.sh
-
-# sudo systemctl daemon-reload
-# sudo systemctl enable lights.service
-# sudo systemctl start lights.service
-# systemctl status lights.service
-
-# sudo systemctl stop lights.service
-# sudo systemctl disable lights.service
